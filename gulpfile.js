e9ea51f800162099d50e3992b6c7712ef9dc79e6
Marcelo Tena Vicente <marcelotena@gmail.com>
2015-02-15 13:06:39 +0100

    Gulp and Gutil installed

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ var gulp = require('gulp'),
     gutil = require('gulp-util');
 
 gulp.task('log', function() {
-    gutil.log('Plantilla base con SASS y Gulp')
+    gutil.log('Plantilla base con SASS y Gulp');
 });
