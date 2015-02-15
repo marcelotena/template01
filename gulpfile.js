d6c3ba23e84371f94bfdcaa9f0bb29c98ae90732
Marcelo Tena Vicente <marcelotena@gmail.com>
2015-02-15 17:55:18 +0100

    added compass sources

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var jsSources = [
     //añade aquí archivos .js para concatenar en el script final
     //'components/scripts/*******.js'
 ];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'))
 });
